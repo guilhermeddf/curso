44ac92d60949d041776dedb1e1904a33eba5188e
Guilherme Freitas <guilhermedin@gmail.com>
2019-12-19 12:44:38 -0300

    configurando o h2

--- src/main/java/br/guilherme/curso/Domain/Category.java
+++ src/main/java/br/guilherme/curso/Domain/Category.java
@@ -1,12 +1,21 @@
 package br.guilherme.curso.Domain;
 
 
+import net.bytebuddy.dynamic.loading.InjectionClassLoader;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import java.io.Serializable;
 import java.util.Objects;
 
+@Entity
 public class Category implements Serializable {
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String name;
 
