c035574ba0bf7f2ad5ccd3eb0ec9b7b9392cfd18
Guilherme Freitas <guilhermedin@gmail.com>
2019-12-19 12:11:24 -0300

    categoria resource criada

---
+++ src/main/java/br/guilherme/curso/Resources/CategoryResource.java
@@ -0,0 +1,16 @@
+package br.guilherme.curso.Resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/categories")
+public class CategoryResource {
+
+    @RequestMapping(method = RequestMethod.GET)
+    public String list(){
+        return "Ta ligado";
+    }
+
+}
