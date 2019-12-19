51ea94c7278b45f549aa1108a8fae49da067a4c6
Guilherme Freitas <guilhermedin@gmail.com>
2019-12-19 13:45:25 -0300

    instanciando

--- src/main/java/br/guilherme/curso/CursoApplication.java
+++ src/main/java/br/guilherme/curso/CursoApplication.java
@@ -1,13 +1,29 @@
 package br.guilherme.curso;
 
+import br.guilherme.curso.domain.Category;
+import br.guilherme.curso.repositories.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursoApplication {
+import java.util.Arrays;
 
-	public static void main(String[] args) {
+@SpringBootApplication
+public class CursoApplication implements CommandLineRunner {
+	@Autowired
+	private CategoryRepository categoryRepository;
+
+	public static void main(String[] args)  {
 		SpringApplication.run(CursoApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Category cat1 = new Category(1, "Informatica");
+		Category cat2 = new Category(2, "Escritorio");
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
+
+		
+	}
 }
--- src/main/java/br/guilherme/curso/domain/Category.java
+++ src/main/java/br/guilherme/curso/domain/Category.java
@@ -17,6 +17,8 @@ public class Category implements Serializable {
     private int id;
     private String name;
 
+    public Category(){}
+
     public Category(int id, String name) {
         this.id = id;
         this.name = name;
