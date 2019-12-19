403ffb0341c350ba2209ff00a68a9ec0ddb1a736
Guilherme Freitas <guilhermedin@gmail.com>
2019-12-19 12:27:52 -0300

    categoria domain add

--- src/main/java/br/guilherme/curso/Resources/CategoryResource.java
+++ src/main/java/br/guilherme/curso/Resources/CategoryResource.java
@@ -1,16 +1,28 @@
 package br.guilherme.curso.Resources;
 
+import br.guilherme.curso.Domain.Category;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categories")
 public class CategoryResource {
 
     @RequestMapping(method = RequestMethod.GET)
-    public String list(){
-        return "Ta ligado";
+    public List<Category> list(){
+        Category cat1 = new Category(1, "informatica");
+        Category cat2 = new Category(2, "escritorio");
+
+        List<Category> list = new ArrayList<>();
+        list.add(cat1);
+        list.add(cat2);
+
+
+        return list;
     }
 
 }
